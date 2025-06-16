7eab031c4340b355f3787a301c3fb4f0b39c2a08
nngtkhngoc <khanhngoc230705@gmail.com>
2025-06-16 13:40:08 +0700

    feat(reviews): add getReviewsByUserId API

--- src/main/java/org/example/qlthuvien/controller/ReviewController.java
+++ src/main/java/org/example/qlthuvien/controller/ReviewController.java
@@ -3,6 +3,7 @@ package org.example.qlthuvien.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.qlthuvien.dto.notification.NotificationResponse;
 import org.example.qlthuvien.dto.review.ReviewResponse;
+import org.example.qlthuvien.entity.User;
 import org.example.qlthuvien.mapper.NotificationMapper;
 import org.example.qlthuvien.mapper.ReviewMapper;
 import org.example.qlthuvien.payload.ApiResponse;
@@ -11,6 +12,7 @@ import org.example.qlthuvien.repository.ReviewRepository;
 import org.example.qlthuvien.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +37,16 @@ public class ReviewController {
         return ResponseEntity.ok(new ApiResponse<>(true, "Xem review thành công.", reviews));
     }
 
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getNotificationsByUserId(@PathVariable Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy người dùng."));
+
+        List<ReviewResponse> userNotis = reviewRepository.findByUserIdOrderByCreatedAtDesc(user.getId())
+                .stream()
+                .map(reviewMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(new ApiResponse<>(true, "Xem thông báo của người dùng thành công.", userNotis));
+    }
 }
