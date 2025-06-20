61bfc7a4cce052de44562bbb3cc49e8d7ccfd10e
Ranizt <chicuongvo3@gmail.com>
2025-06-20 20:54:02 +0700

    Add api endpoint to get number of active users

--- src/main/java/org/example/qlthuvien/config/WebSocketConfig.java
+++ src/main/java/org/example/qlthuvien/config/WebSocketConfig.java
@@ -1,12 +1,23 @@
 package org.example.qlthuvien.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
--- src/main/java/org/example/qlthuvien/controller/ChatController.java
+++ src/main/java/org/example/qlthuvien/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.example.qlthuvien.entity.ChatMessage;
 import org.example.qlthuvien.entity.User;
 import org.example.qlthuvien.mapper.MessageMapper;
 import org.example.qlthuvien.repository.ChatMessageRepository;
+import org.example.qlthuvien.services.ActiveUserTracker;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,13 +26,14 @@ public class ChatController {
     private final ChatMessageRepository messageRepository;
     private final MessageMapper messageMapper;
     private final EntityManager entityManager;
+    private final ActiveUserTracker activeUserTracker;
+
 
     @MessageMapping("/chat.sendMessage") // client gửi vào /app/chat.sendMessage
     @SendTo("/topic/public")
     // broadcast tới tất cả client đang sub /topic/public
     public MessageResponse sendMessage(CreateMessage message) {
         User user = entityManager.find(User.class, message.getSender_id());
-
         ChatMessage messageEntity = new ChatMessage();
         messageEntity.setSenderId(user.getId());
         messageEntity.setImage(user.getImage());
@@ -40,6 +42,13 @@ public class ChatController {
         ChatMessage newMessage = messageRepository.save(messageEntity);
         MessageResponse res = messageMapper.toResponse(newMessage);
         return res;
+    }
+
+
+
+    @GetMapping("/api/chat/active-users")
+    public int getActiveUserCount() {
+        return activeUserTracker.getActiveSessionCount();
     }
 
     @GetMapping("/api/chat/messages")
@@ -79,7 +88,5 @@ public class ChatController {
         return ResponseEntity.noContent().build(); // 204 No Content indicates successful deletion
     }
 
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
+
 }
