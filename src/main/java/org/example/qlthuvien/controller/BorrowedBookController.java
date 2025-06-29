5e91da5a756fb7e5c5f3e0f17b6b418fd3648e5b
Ranizt <chicuongvo3@gmail.com>
2025-06-29 20:06:29 +0700

    Use controller to update book_item instead of repo

--- src/main/java/org/example/qlthuvien/controller/BorrowedBookController.java
+++ src/main/java/org/example/qlthuvien/controller/BorrowedBookController.java
@@ -3,6 +3,7 @@ package org.example.qlthuvien.controller;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.example.qlthuvien.dto.bookitem.STATUS;
+import org.example.qlthuvien.dto.bookitem.UpdateBookItemRequest;
 import org.example.qlthuvien.dto.borrowedbook.BorrowedBookResponse;
 import org.example.qlthuvien.dto.borrowedbook.CreateBorrowedBookRequest;
 import org.example.qlthuvien.dto.borrowedbook.UpdateBorrowedBookRequest;
@@ -32,6 +33,7 @@ public class BorrowedBookController {
     private final BookItemRepository bookItemRepository;
     private final BorrowedBookMapper borrowedBookMapper;
     private final BadgeService badgeService;
+    private final BookItemController bookItemController;
 
     @GetMapping
     public List<BorrowedBookResponse> getAllBorrowedBooks() {
@@ -61,8 +63,10 @@ public class BorrowedBookController {
 
         userService.addXp(borrowUser, 5);
         badgeService.checkBadges(borrowUser.getId());
-        bookItem.setStatus(STATUS.Borrowed);
-        bookItemRepository.save(bookItem);
+        UpdateBookItemRequest updateBookItemRequest = new UpdateBookItemRequest();
+        updateBookItemRequest.setStatus(STATUS.Borrowed);
+        bookItemController.updateBookItem(bookItem.getId(), updateBookItemRequest);
+
 
         return borrowedBookMapper.toResponse(saved);
     }
@@ -77,18 +81,18 @@ public class BorrowedBookController {
         BorrowedBook temp = borrowedBookMapper.toEntity(data);
         BorrowedBook updated = borrowedBookRepository.save(existing);
         BookItem bookItem = existing.getBook_item();
+        UpdateBookItemRequest updateBookItemRequest = new UpdateBookItemRequest();
         if (updated.getStatus() == LendingStatus.BORROWED) {
-            bookItem.setStatus(STATUS.Borrowed);
-
+            updateBookItemRequest.setStatus(STATUS.Borrowed);
         }
         if (updated.getStatus() == LendingStatus.PENDING) {
-            bookItem.setStatus(STATUS.Borrowed);
+            updateBookItemRequest.setStatus(STATUS.Borrowed);
 
         }
         if (updated.getStatus() == LendingStatus.RETURNED) {
-            bookItem.setStatus(STATUS.AVAILABLE);
+            updateBookItemRequest.setStatus(STATUS.AVAILABLE);
         }
-        bookItemRepository.save(bookItem);
+        bookItemController.updateBookItem(bookItem.getId(), updateBookItemRequest);
         return borrowedBookMapper.toResponse(updated);
     }
 
@@ -100,7 +104,9 @@ public class BorrowedBookController {
             BookItem item = borrowedBook.getBook_item();
             if (item != null) {
                 item.setBorrowedBook(null); // cắt mối quan hệ
-                item.setStatus(STATUS.AVAILABLE);
+                UpdateBookItemRequest updateBookItemRequest = new UpdateBookItemRequest();
+                updateBookItemRequest.setStatus(STATUS.AVAILABLE);
+                bookItemController.updateBookItem(item.getId(), updateBookItemRequest);
             }
             borrowedBookRepository.deleteById(id);
             response.put("message", "Borrowed book successfully deleted");
